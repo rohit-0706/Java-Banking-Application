6f2fc6a23d5197348c420cdf1b5fb5db87dd60a1
rohitverma <rv16verma@gmail.com>
2023-08-14 19:39:32 +0530

    added dependency and description for swagger page

--- src/main/java/com/rohit/bankingProject/BankingProjectApplication.java
+++ src/main/java/com/rohit/bankingProject/BankingProjectApplication.java
@@ -1,9 +1,36 @@
 package com.rohit.bankingProject;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import jdk.jfr.Description;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info=@Info(
+				title = "Rohit Banking application",
+				description = "Backend Rest APIs for this project",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Rohit Verma",
+						email = "rv16verma@gmail.com",
+						url = "https://github.com/rohit-0706/Java-Banking-Application"
+				),
+				license = @License(
+						name = "Rohit Banking Application",
+						url = "https://github.com/rohit-0706/Java-Banking-Application"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Rohit Banking Application Documentation",
+				url = "https://github.com/rohit-0706/Java-Banking-Application"
+
+		)
+)
 public class BankingProjectApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/rohit/bankingProject/controller/UserController.java
+++ src/main/java/com/rohit/bankingProject/controller/UserController.java
@@ -2,15 +2,23 @@ package com.rohit.bankingProject.controller;
 
 import com.rohit.bankingProject.dto.*;
 import com.rohit.bankingProject.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = "User Account Management APIs")
 public class UserController {
 
     @Autowired
     UserService userService;
+    @Operation(
+            summary = "Create New User Account",
+            description = "Creating a new and assigning an account ID"
+    )
+
 
 
     @PostMapping
