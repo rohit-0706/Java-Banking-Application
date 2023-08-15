e913139720b9c9f4996f3b5e76de6ef7a1cdf769
rohitverma <rv16verma@gmail.com>
2023-08-15 15:34:02 +0530

    added createdat and modified at on transaction

--- src/main/java/com/rohit/bankingProject/entity/Transaction.java
+++ src/main/java/com/rohit/bankingProject/entity/Transaction.java
@@ -2,8 +2,11 @@ package com.rohit.bankingProject.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
 @Getter
@@ -21,5 +24,10 @@ public class Transaction {
     private BigDecimal amount;
     private String accountNumber;
     private String status;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime modifiedAt;
+
 
 }
